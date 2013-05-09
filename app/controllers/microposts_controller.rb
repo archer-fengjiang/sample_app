47243d33b4d6313b0e7a29c42f0002900a6ec6e6
Fengjiang Li <archer@il0103a-dhcp39.apple.com>
2013-05-09 11:27:15 -0700

    micropost done

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_filter :signed_in_user
+  before_filter :signed_in_user,  only: [:create, :destroy]
+  before_filter :correct_user,    only: :destroy
 
   def index
   end
@@ -16,5 +17,16 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_url
   end
+
+  private 
+    def correct_user
+      # we use find_ny_id instead of find because find raises an exception when the micropost doesn't exits
+      # in that case, use "rescue redirect_to root_url" 
+      # finy_by_id will return nil
+      @micropost = current_user.microposts.find_by_id(params[:id])
+      redirect_to root_url unless current_user?(@micropost.user)
+    end
 end
--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -30,4 +30,53 @@ describe "Micropost Pages" do
       end
     end
   end
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect{ click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+    end
+  end
+
+  describe "micropost side bar count" do
+    describe "initially should be zero" do
+      before { visit root_path }
+      it { should have_content "0 micropost"}
+    end
+
+    describe "creating one should change count to one" do
+      before do
+        FactoryGirl.create(:micropost, user: user)
+        visit root_path
+      end
+      it { should have_content "1 micropost" }
+    end
+
+    describe "creating two should change count two and make microposts to be perlur" do
+      before do
+        2.times { FactoryGirl.create(:micropost, user: user) }
+        visit root_path
+      end
+      it { should have_content "2 microposts"}
+    end
+  end
+
+  describe "pagination" do
+    before do
+      31.times { FactoryGirl.create(:micropost, user: user) }
+      visit root_path
+    end
+
+    it { should have_selector('div.pagination') }
+    it "should list each micropost" do
+      Micropost.paginate(page: 1).each do |micropost|
+        page.should have_selector('li', text: micropost.content)
+      end
+    end
+  end
 end
